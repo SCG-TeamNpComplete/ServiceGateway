112297e7b388900f99b3e3bc236613747929e6eb
Soumya Achar <soumya@onlab.us>
2016-11-17 18:30:43 -0500

    remove

--- scripts/install.sh
+++
@@ -1,5 +0,0 @@
-echo 'starting installation process'
-cd '/home/ec2-user/docker'
-sudo docker login -e="kedar.gn20@gmail.com" -u="kedargn" -p="npcomplete"   #TODO : hide password
-sudo docker pull kedargn/dataingestortest
-sudo docker run -d -p 8085:8080 --name dataingestortest $(sudo docker images | grep kedargn/dataingestortest | awk '{print $3}') >> ./log.txt
