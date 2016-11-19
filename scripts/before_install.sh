f2cedce358b9bedb51c956e06e450bc8ebec04e2
Kedar <kedar.gn20@gmail.com>
2016-11-19 00:09:03 -0500

    code for #3

---
+++ scripts/before_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo yum install -y docker-io
+sudo service docker start
---
+++ scripts/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd '/home/ec2-user/docker'
+sudo docker login -u="kedargn" -p="npcomplete"   #TODO : hide password
+sudo docker pull kedargn/servicegateway
+
+no_of_instances=1     #change this to set the number of instances
+current_instance=1
+port=11000
+while [ $current_instance -le $no_of_instances ]
+do
+	#echo "$(sudo docker ps -a | grep "forecastdetector$current_instance" | awk '{print $1}')"
+	if [ "$(sudo docker ps -a | grep "servicegateway$current_instance" | awk '{print $1}')" != "" ]; then
+		sudo docker ps -a | grep "servicegateway$current_instance" | awk '{print $1}' | xargs --no-run-if-empty sudo docker stop
+		sudo docker ps -a | grep "servicegateway$current_instance" | awk '{print $1}' | xargs --no-run-if-empty sudo docker rm
+	fi
+	#echo "$port:$port"
+	sudo docker run -d -p "$port:$port" --name "servicegateway$current_instance" $(sudo docker images | grep kedargn/servicegateway | awk '{print $3}')
+	echo "servicegateway$current_instance instance"
+	current_instance=$((current_instance+1))
+	port=$((port+1))
+	sleep 1
+done
